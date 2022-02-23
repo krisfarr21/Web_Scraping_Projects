0e34961e23359489abb3ec7b90127c78751c9ccf
krisfarr21 <krisfarr21@gmail.com>
2022-02-23 10:37:57 +0100

    progress: managed to follow pagination of one category - have to figure out how to move to the next category and continue following pagination

--- prices/bargain_prices/bargain_prices/spiders/prices.py
+++ prices/bargain_prices/bargain_prices/spiders/prices.py
@@ -1,65 +1,49 @@
 from multiprocessing.sharedctypes import Value
 import scrapy
 from scrapy_splash import SplashRequest
-from bargain_prices.items import BargainPricesItem # for future itemloader
+import os
+# from bargain_prices.items import BargainPricesItem # for future itemloader
 
+# NEXT TASK: send requests for each category + pages
 
 class prices_spider(scrapy.Spider):
-    name =  'prices'
-    local_host = 'http://localhost:8050/render.html?url='
-    start_url = 'https://www.greens.com.mt/products?cat=fruitsandvegetables&pg=15'
+    name = 'prices'
+    THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+    FILE = os.path.join(THIS_FOLDER, 'greens_links.txt')
+    
+    start_urls = []
+
+    with open(FILE, 'r') as f:
+        lines = f.readlines()
+        URL_NUMBER = '&pg=1&sort=Position&sortd=Asc'
+        urls = [link.strip('\n') for link in lines]
+        for link in urls:
+            start_urls.append(link+URL_NUMBER)
+        start_urls = start_urls[2:4]
+        # URL_NUMBER not identified in list comprehension????
+        # start_urls = [link + URL_NUMBER for link in start_urls]
 
     def start_requests(self):
-        yield SplashRequest(url=self.start_url, callback=self.parse, args={'wait': 2})
+        for url in self.start_urls:
+            yield SplashRequest(url=url, callback=self.parse, args={'wait': 4})
 
-    def check_next_url(self, response):
-        '''
-        Checks if next page exists
-        '''
-        next_page = response.xpath('//*[@id="pnlPagesTop"]/a[4]/@href').extract()
-        check_product = response.css('a.title.product-title::text').get()
-        if next_page:
-            return next_page[0]
-        return False
-    
-    def parse(self, response, **kwargs):
-        # yield requests for each page
+    def parse(self, response):
+        print("START-URLS ---> ", self.start_urls)
+        current_category = response.xpath('//*[@id="ddHeaderSearchDropdown"]/option[10]/text()').extract()[0]
         for product in response.css('div.col-md-2.col-sm-3.shop-grid-item'):
             yield { 
+            'category' : current_category,
             'product_name' : product.css('a.title.product-title::text').get(),
             'product_price' : product.css('div.current::text').get(),
+            'product_type' : product.css('a.tag::text').get(),
+            'offer_type': product.css('div.hot-mark.purple::text').get(),
+            'offer': product.css('div.special-offer-text::text').get()
             }
-
+        next_page = response.xpath('//*[@id="pnlPagesTop"]/a[4]/@href').extract()
         try:
-            next_page = self.check_next_url(response=response)
+            print("PAGNA LI JMISS--->",next_page, len(next_page), type(next_page[0]))
         except:
-            raise 'Next page not found. End of category'
-        finally:
-            yield SplashRequest(url=next_page, callback=self.parse, args={'wait': 2})
-
-        # next_page = response.css('#pnlPagesBottom > a:nth-child(5)::attr(href)').extract_first()
-        # if next_page:
-        #     yield response.follow(url= 'http://localhost:8050/render.html?url=' + next_page + '&wait=4', 
-        #     callback=self.parse)
-        # products = response.css('div.col-md-2.col-sm-3.shop-grid-item')
-        # for product in products:
-        #     item = { 
-        #     'product_name' : product.css('a.title.product-title::text').get(),
-        #     'product_price' : product.css('div.current::text').get(),
-        #     }
-        #     yield item
-
-        # following pagitation link
-        # next_page_url = response.css('#pnlPagesBottom > a:nth-child(5)::attr(href)').extract_first()
-        # if next_page_url:
-        #     # print(next_page_url)
-        #     yield SplashRequest(url=next_page_url, callback=self.parse, args={'wait': 4}),
-
-        
-
-        # item = BargainPricesItem()
-        # item['next_page'] =  response.xpath('//*[@id="pnlPagesBottom"]/a[4]/@href')
-        # print("ITEM-->",item)
-        # print("ITEM_NEXT_PAGE-->",item['next_page'].get(), type(item['next_page']))
-        # if item['next_page'] is not None:
-        #     yield response.follow(item['next_page'], self.parse)
+            print("PAGNA LI JMISS--->",next_page, len(next_page), type(next_page))
+        # arbitrary number - last page of above xpath was outputting '#' instead of None
+        if len(next_page[0]) > 5 and next_page[0] is not None: 
+            yield SplashRequest(url=next_page[0], callback=self.parse, args={'wait': 4})
