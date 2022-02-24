4cd03e3b5a634d0f63060ebd0fbbdc3cb3da9ae7
krisfarr21 <krisfarr21@gmail.com>
2022-02-24 13:06:55 +0100

    added categories feature to class; will write script through main function to execute each category

--- prices/bargain_prices/bargain_prices/spiders/prices.py
+++ prices/bargain_prices/bargain_prices/spiders/prices.py
@@ -2,37 +2,30 @@ from multiprocessing.sharedctypes import Value
 import scrapy
 from scrapy_splash import SplashRequest
 import os
+import regex as re
+from bargain_prices.spiders.links.get_hrefs import green_categories
+
 # from bargain_prices.items import BargainPricesItem # for future itemloader
 
 # NEXT TASK: send requests for each category + pages
 
 class prices_spider(scrapy.Spider):
-    name = 'prices'
-    THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
-    FILE = os.path.join(THIS_FOLDER, 'greens_links.txt')
+    name = 'greens_prices'
     
-    start_urls = []
-
-    with open(FILE, 'r') as f:
-        lines = f.readlines()
-        URL_NUMBER = '&pg=1&sort=Position&sortd=Asc'
-        urls = [link.strip('\n') for link in lines]
-        for link in urls:
-            start_urls.append(link+URL_NUMBER)
-        start_urls = start_urls[2:4]
-        # URL_NUMBER not identified in list comprehension????
-        # start_urls = [link + URL_NUMBER for link in start_urls]
+    urls_categories = green_categories()
+    
+    def __init__(self, category=None, *args, **kwargs):
+        super(prices_spider, self).__init__(*args, **kwargs)
+        category = self.urls_categories[category]
+        self.start_urls = category
 
     def start_requests(self):
-        for url in self.start_urls:
-            yield SplashRequest(url=url, callback=self.parse, args={'wait': 4})
+        yield SplashRequest(url=self.start_urls, callback=self.parse, args={'wait': 4})
 
     def parse(self, response):
-        print("START-URLS ---> ", self.start_urls)
-        current_category = response.xpath('//*[@id="ddHeaderSearchDropdown"]/option[10]/text()').extract()[0]
+        print(self.logger.info('A response from %s just arrived!', response.url))
         for product in response.css('div.col-md-2.col-sm-3.shop-grid-item'):
             yield { 
-            'category' : current_category,
             'product_name' : product.css('a.title.product-title::text').get(),
             'product_price' : product.css('div.current::text').get(),
             'product_type' : product.css('a.tag::text').get(),
@@ -47,3 +40,46 @@ class prices_spider(scrapy.Spider):
         # arbitrary number - last page of above xpath was outputting '#' instead of None
         if len(next_page[0]) > 5 and next_page[0] is not None: 
             yield SplashRequest(url=next_page[0], callback=self.parse, args={'wait': 4})
+
+# class prices_spider(scrapy.Spider):
+#     name = 'prices-COPY'
+#     THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+#     FILE = os.path.join(THIS_FOLDER, 'greens_links.txt')
+    
+#     start_urls = []
+
+#     with open(FILE, 'r') as f:
+#         lines = f.readlines()
+#         URL_NUMBER = '&pg=1&sort=Position&sortd=Asc'
+#         urls = [link.strip('\n') for link in lines]
+#         for link in urls:
+#             start_urls.append(link+URL_NUMBER)
+#         start_urls = start_urls[2:4]
+#         # URL_NUMBER not identified in list comprehension????
+#         # start_urls = [link + URL_NUMBER for link in start_urls]
+
+#     def start_requests(self):
+#         for url in self.start_urls:
+#             yield SplashRequest(url=url, callback=self.parse, args={'wait': 4})
+
+#     def parse(self, response):
+#         print(self.logger.info('A response from %s just arrived!', response.url))
+#         print("START-URLS ---> ", self.start_urls)
+#         current_category = response.xpath('//*[@id="ddHeaderSearchDropdown"]/option[10]/text()').extract()[0]
+#         for product in response.css('div.col-md-2.col-sm-3.shop-grid-item'):
+#             yield { 
+#             'category' : current_category,
+#             'product_name' : product.css('a.title.product-title::text').get(),
+#             'product_price' : product.css('div.current::text').get(),
+#             'product_type' : product.css('a.tag::text').get(),
+#             'offer_type': product.css('div.hot-mark.purple::text').get(),
+#             'offer': product.css('div.special-offer-text::text').get()
+#             }
+#         next_page = response.xpath('//*[@id="pnlPagesTop"]/a[4]/@href').extract()
+#         try:
+#             print("PAGNA LI JMISS--->",next_page, len(next_page), type(next_page[0]))
+#         except:
+#             print("PAGNA LI JMISS--->",next_page, len(next_page), type(next_page))
+#         # arbitrary number - last page of above xpath was outputting '#' instead of None
+#         if len(next_page[0]) > 5 and next_page[0] is not None: 
+#             yield SplashRequest(url=next_page[0], callback=self.parse, args={'wait': 4})
