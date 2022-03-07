bec904a76d9b35e81ae66198e0d428766ce491ca
krisfarr21 <krisfarr21@gmail.com>
2022-03-07 21:30:17 +0100

    added new settings; crawling 5-6 times faster however missing some values

--- news/news/settings.py
+++ news/news/settings.py
@@ -17,9 +17,21 @@ NEWSPIDER_MODULE = 'news.spiders'
 SPLASH_URL = 'http://localhost:8050'
 
 DOWNLOADER_MIDDLEWARES = {
+
+    # scrapy splash middlewars
     'scrapy_splash.SplashCookiesMiddleware': 723,
     'scrapy_splash.SplashMiddleware': 725,
     'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 810,
+
+    # to make scrapy faster (https://stackoverflow.com/questions/56783028/how-to-speed-up-scrapy)
+#     'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
+#     'scrapy.spidermiddlewares.referer.RefererMiddleware': 80,
+#     'scrapy.downloadermiddlewares.retry.RetryMiddleware': 90,
+#     'scrapy_fake_useragent.middleware.RandomUserAgentMiddleware': 120,
+#     'scrapy.downloadermiddlewares.cookies.CookiesMiddleware': 130,
+#     'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 810,
+#     'scrapy.downloadermiddlewares.redirect.RedirectMiddleware': 900,
+#     'scraper.middlewares.ScraperDownloaderMiddleware': 1000
 }
 
 SPIDER_MIDDLEWARES = {
@@ -36,6 +48,27 @@ HTTPCACHE_STORAGE = 'scrapy_splash.SplashAwareFSCacheStorage'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
+
+# to make scrapy faster (https://stackoverflow.com/questions/56783028/how-to-speed-up-scrapy)
+
+DOWNLOAD_DELAY = 0
+DOWNLOAD_TIMEOUT = 30
+RANDOMIZE_DOWNLOAD_DELAY = True
+
+REACTOR_THREADPOOL_MAXSIZE = 128
+CONCURRENT_REQUESTS = 256
+CONCURRENT_REQUESTS_PER_DOMAIN = 256
+CONCURRENT_REQUESTS_PER_IP = 256
+
+AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_START_DELAY = 1
+AUTOTHROTTLE_MAX_DELAY = 0.25
+AUTOTHROTTLE_TARGET_CONCURRENCY = 128
+AUTOTHROTTLE_DEBUG = True
+
+RETRY_ENABLED = True
+RETRY_TIMES = 3
+RETRY_HTTP_CODES = [500, 502, 503, 504, 400, 401, 403, 404, 405, 406, 407, 408, 409, 410, 429]
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
